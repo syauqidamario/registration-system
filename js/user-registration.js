ff9abfb0e04b308f2f6623a56fea87bbf46be29d
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-06-07 16:44:50 +0700

    Changes in the front-end again.

--- js/user-registration.js
+++ js/user-registration.js
@@ -9,58 +9,35 @@ var firebaseConfig = {
       measurementId: "G-Z9G1WHDDRL"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // Get a reference to the Firebase Firestore database
 var database = firebase.firestore();
 
-// Access the form elements
-var form1 = document.getElementById("registration-form");
-var form2 = document.getElementById("registration-form");
+// Access the form element
+var form = document.getElementById("registration-form");
 
-form1.addEventListener("submit", function(e) {
-  e.preventDefault(); // Prevent form submission
+if (form) {
+  form.addEventListener("submit", function(e) {
+    e.preventDefault(); // Prevent form submission
 
-  // Get values from the form
-  var name = document.getElementById("name1").value;
-  var email = document.getElementById("person_name1").value;
-  var userType = ""; // Modify this line if necessary
+    // Get values from the form
+    var name = document.getElementById("name1").value;
+    var email = document.getElementById("person_name1").value;
 
-  // Store new registration data in the database
-  database.collection("registrations").add({
-    name: name,
-    email: email,
-    userType: userType
-  })
-    .then(function() {
-      alert("Registration successful!");
-      form1.reset(); // Reset the form
+    // Store new registration data in the database
+    database.collection("registrations").add({
+      name: name,
+      email: email
     })
-    .catch(function(error) {
-      console.error("Error adding registration: ", error);
-    });
-});
-
-form2.addEventListener("submit", function(e) {
-  e.preventDefault(); // Prevent form submission
-
-  // Get values from the form
-  var name = document.getElementById("name2").value;
-  var email = document.getElementById("person_name2").value;
-  var userType = ""; // Modify this line if necessary
-
-  // Store new registration data in the database
-  database.collection("registrations").add({
-    name: name,
-    email: email,
-    userType: userType
-  })
-    .then(function() {
-      alert("Registration successful!");
-      form2.reset(); // Reset the form
-    })
-    .catch(function(error) {
-      console.error("Error adding registration: ", error);
-    });
-});
+      .then(function() {
+        alert("Registration successful!");
+        form.reset(); // Reset the form
+      })
+      .catch(function(error) {
+        console.error("Error adding registration: ", error);
+      });
+  });
+} else {
+  console.error("Registration form element not found.");
+}
